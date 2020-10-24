2a33168067d824a09d7a0c825df1893cd8e5fc82
DedAdan <haynmine@gmail.com>
2020-10-24 11:48:38 -0500

    chain added

---
+++ chain.js
@@ -0,0 +1,19 @@
+class Chain{
+    constructor(bodyA,bodyB)
+    {
+    var options={
+        bodyA: bodyA,
+        bodyB: bodyB,
+        stiffness: 0.04,
+        length: 10
+    }    
+    this.chain= Matter.Constraint.create(options)
+    World.add(world,this.chain)
+    }
+    display(){
+        var A=this.chain.bodyA.position
+        var B=this.chain.bodyB.position
+        strokeWeight(5)
+         line(A.x,A.y,B.x,B.y) 
+    }
+}
